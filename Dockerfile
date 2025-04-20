f16d1b9e8951a7b380872ef011b6d056f4a6d924
cgjgh <160297365+cgjgh@users.noreply.github.com>
2025-04-20 18:06:08 -0500

    Add projects with env git config

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,21 @@
 FROM nodered/node-red:latest
-COPY package.json /data/package.json
+
+# Copy package.json with correct ownership and install dependencies
+COPY --chown=node-red:node-red package.json /data/package.json
 RUN cd /data && npm install
-COPY flows.json /data/flows.json
-COPY settings.js /data/settings.js
+
+# Copy settings.js with correct ownership
+COPY --chown=node-red:node-red settings.js /data/settings.js
+
+# Copy all files prefixed with .config with correct ownership
+COPY --chown=node-red:node-red .config* /data/
+
+# Create the empty project directory with correct ownership
+RUN mkdir -p /data/projects/nodered_render && chown node-red:node-red /data/projects/nodered_render
+
+# Copy the custom entrypoint script
+COPY --chown=node-red:node-red entrypoint.sh /data/entrypoint.sh
+RUN chmod +x /data/entrypoint.sh
+
+# Set the custom script as the new entrypoint
+ENTRYPOINT ["/data/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Hash the Node-RED admin password using bcrypt and store it in an environment variable.
+# This is used for securing the Node-RED admin interface.
+export NODE_RED_ADMIN_HASHED_PASS=$(node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" "$NODE_RED_ADMIN_PASS")
+
+# Replace the placeholder for the Node-RED credential secret in the configuration file.
+# This ensures that the credential secret is set correctly for Node-RED projects.
+sed -i "s|NODE_RED_CREDENTIAL_SECRET|$NODE_RED_CREDENTIAL_SECRET|g" /data/.config.projects.json
+
+# Check if the Node-RED project repository has already been cloned.
+# If not, clone the repository from GitHub using the provided token and repository name.
+if [ ! -d /data/projects/nodered_render/.git ]; then
+    git clone https://$GITHUB_TOKEN@github.com/$REPO_FULL_NAME.git /data/projects/nodered_render
+fi
+
+# Configure the Git user name and email for the cloned repository.
+# This is necessary for any Git operations that require user information.
+git -C /data/projects/nodered_render config user.name "$GIT_USER_NAME"
+git -C /data/projects/nodered_render config user.email "$GIT_USER_EMAIL"
+
+# Execute the Node-RED entrypoint script with any additional arguments passed to this script.
+# This starts the Node-RED application.
+exec /usr/src/node-red/entrypoint.sh "$@"
